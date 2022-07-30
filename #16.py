f24fd1ab36a66e38b38654d8ee9f3a0e9a7a974d
laurore1998 <laurorenicodem@gmail.com>
2022-07-30 16:02:00 -0400

    update 16

--- #16.py
+++ #16.py
@@ -1,16 +1,17 @@
 import string
 a=string.ascii_uppercase
-#b="<1N <7H <0T <3S >2J <6U <5Y >8D >5D >1A"
+
 b="<1N <7H <0T <3S >2J <6U <5Y >8D >5D >1A"
 #print(b[0:][1+3])
 b=b.split()
+s=""
 for i in range(len(b)):
     if b[i][0] == ">":
-        #print(a.index(b[i][2]) + int(b[i][1]))
-        print(a[a.index(b[i][2]) + int(b[i][1])])
+        s+=a[a.index(b[i][2]) + int(b[i][1])]
     else:
-        #print(a.index(b[i][2]) - int(b[i][1]))
-        print(a[a.index(b[i][2]) - int(b[i][1])])
+        s+=a[a.index(b[i][2]) - int(b[i][1])]
+
+print(s)
 
 
 
--- #5.py
+++ #5.py
@@ -1,12 +1,12 @@
 a = "5 45 123 12"
-"""x = a.split()
+x = a.split()
 p = x[1]
 xs = x[2]
 j = x[3]
 s = 0
 k = 0
 d = 0
-for i in range(len(p)):
+"""for i in range(len(p)):
     s += int(p[i])
 for f in range(len(xs)):
     k += int(xs[f])
