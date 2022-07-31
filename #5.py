77ea6ecbda34c138824fd6c82b956dd6f907e42b
laurore1998 <laurorenicodem@gmail.com>
2022-07-31 01:23:20 -0400

    correction #5 fini

--- #5.py
+++ #5.py
@@ -1,18 +1,18 @@
 a = "5 45 123 12"
 a = a.split()
 lisadd = []
-y = 0
+number_in_a = 0
 t=1
 
 for y in range(len(a)):
     z=0
-    for i in a[y]:
+    for i in a[number_in_a]:
         z += int(i)
     lisadd.append(z)
 
 for u in lisadd:
     t*=u
-    
+
 
 #print(lis)
 print(t)
